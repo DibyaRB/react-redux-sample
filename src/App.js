83648d1e803df0194d6762e06d6e786d8b739579
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-12-15 00:09:29 +0530

    Implementing actions and modifying reducers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment,decrement} from './actions'
 
 function App() {
   const counter = useSelector(state=> state.counter);
-  const isLogged = useSelec
-
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1> Counter {counter}</h1>
 
-      <h3> Valuable Information I shouldn't see</h3>
+      <button onClick={ () => dispatch(increment(5))}> + </button>
+      <button onClick={ () => dispatch(decrement())}> - </button>
+
+      {isLogged ? <h3> Valuable Information I shouldn't see</h3> :'' }
     </div>
   );
 }
